f9859003710d077745dcd20fdcd6ca373fc0f8f5
Shakti Singh <sk.shrinet@gmail.com>
2022-05-19 23:33:46 +0530

    fix(master): testing master commit

--- rest/src/main/java/org/penta/work/boostrap/rest/exception/ExceptionHandler.java
+++ rest/src/main/java/org/penta/work/boostrap/rest/exception/ExceptionHandler.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -13,10 +12,9 @@ import java.sql.SQLIntegrityConstraintViolationException;
 
 @ControllerAdvice
 @Slf4j
-public class RestResponseEntityExceptionHandler 
-  extends ResponseEntityExceptionHandler {
+public class ExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value
+    @org.springframework.web.bind.annotation.ExceptionHandler(value
       = { IllegalArgumentException.class, IllegalStateException.class })
     protected ResponseEntity<Object> handleConflict(
       RuntimeException ex, WebRequest request) {
@@ -26,7 +24,7 @@ public class RestResponseEntityExceptionHandler
     }
 
 
-    @ExceptionHandler(value
+    @org.springframework.web.bind.annotation.ExceptionHandler(value
             = {SQLIntegrityConstraintViolationException.class })
     protected ResponseEntity<Object> handleJdbcSQLConflict(
             RuntimeException ex, WebRequest request) {
